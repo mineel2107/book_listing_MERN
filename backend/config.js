64e1df64f39f997002175f9ca25f90a997ac913a
Mineel <mineelbunty2002@gmail.com>
2023-10-28 14:58:56 +0530

    Books listing project using MERN stack

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://<username>:<password>@book-store-mern.nydkf7r.mongodb.net/books-collection?retryWrites=true&w=majority";
+
+// use username and password of your mongodb
