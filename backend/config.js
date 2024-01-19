44a32a5f77087483a042fb823627252a0947088d
Mineel <mineelbunty2002@gmail.com>
2024-01-19 16:02:11 +0530

    using .env

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+const username = process.env.USER_NAME;
+const password = process.env.MDB_PASSWORD;
 export const PORT = 5555;
 
-export const mongoDBURL =
-  "mongodb+srv://<username>:<password>@book-store-mern.nydkf7r.mongodb.net/books-collection?retryWrites=true&w=majority";
+export const mongoDBURL = `mongodb+srv://${username}:${password}@book-store-mern.nydkf7r.mongodb.net/?retryWrites=true&w=majority`;
 
 // use username and password of your mongodb
